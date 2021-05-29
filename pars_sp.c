38894fe84359c3d83ed9f1e9b1c0ac74f48539bc
dpatrica <matrixneodev@gmail.com>
2021-05-29 20:33:30 +0300

    first_commit

---
+++ pars_sp.c
@@ -0,0 +1,10 @@
+//
+// Created by neo on 28.05.2021.
+//
+
+#include "sp.h"
+
+int pars_sp(char *sp)
+{
+	return (0);
+}
---
+++ sp.h
@@ -0,0 +1,14 @@
+//
+// Created by neo on 28.05.2021.
+//
+
+#ifndef SP_H
+#define SP_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int pars_sp(char *sp);
+
+#endif
